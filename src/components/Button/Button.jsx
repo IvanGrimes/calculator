fce01edeb725200a447f5471a9e604ccfffdbc46
IvanGrimes <ivan@grimesco.de>
2018-10-04 13:47:55 +0300

    add effect of pressing to the Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,44 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Button.module.css';
 
-const Button = ({
-  handleClick,
-  className,
-  children,
-  twoThird,
-  wide,
-}) => {
-  const classNames = [
-    className,
-    css.number,
-    twoThird ? css.twoThird : '',
-    wide ? css.wide : '',
-  ];
+class Button extends Component {
+  static defaultProps = {
+    className: '',
+    twoThird: false,
+    wide: false,
+    type: 'button',
+  };
 
-  return (
-    <button
-      className={classNames.join(' ').trim()}
-      type="button"
-      onClick={() => handleClick(`${children}`)}
-    >
-      {children}
-    </button>
-  );
-};
+  static propTypes = {
+    className: PropTypes.string,
+    handleClick: PropTypes.func.isRequired,
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    twoThird: PropTypes.bool,
+    wide: PropTypes.bool,
+    type: PropTypes.string,
+  };
 
-Button.defaultProps = {
-  className: '',
-  twoThird: false,
-  wide: false,
-};
+  state = {
+    pressed: false,
+  };
 
-Button.propTypes = {
-  className: PropTypes.string,
-  handleClick: PropTypes.func.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  twoThird: PropTypes.bool,
-  wide: PropTypes.bool,
-};
+  handleMouseDown = () => {
+    this.setState({
+      pressed: true,
+    });
+  };
+
+  handleMouseUp = () => {
+    this.setState({
+      pressed: false,
+    });
+  };
+
+  handleClick = () => {
+    const { handleClick, children } = this.props;
+
+    handleClick(`${children}`);
+  };
+
+  render() {
+    const {
+      className,
+      children,
+      twoThird,
+      wide,
+      type,
+    } = this.props;
+    const { pressed } = this.state;
+    const classNames = [
+      className,
+      css.number,
+      twoThird ? css.twoThird : '',
+      wide ? css.wide : '',
+      pressed ? css.pressed : '',
+    ];
+
+    return (
+      // eslint-disable-next-line react/button-has-type
+      <button
+        className={classNames.join(' ').trim()}
+        type={type}
+        onClick={this.handleClick}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+      >
+        {children}
+      </button>
+    );
+  }
+}
 
 export default Button;
